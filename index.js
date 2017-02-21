342c839c2c8bd0c8c230fe179c9caa95bd8e857f
Fabricio C Zuardi <fabricio@fabricio.org>
2017-02-21 19:45:00 -0300

    basic hello world and build scripts working

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log('HELLO WORLD');
+const Pixi = require('pixi.js');
+const { Application, Text } = Pixi;
 
+const app = new Application();
+const { view, stage } = app;
+
+const text = new Text('Hello World', {
+    fontFamily: 'Arial',
+    fontSize: 70,
+    fill: 0xffff00,
+    align: 'center'
+});
+
+const screenCenter = () => [window.screen.width / 2, window.screen.height / 2];
+
+text.position.set(...screenCenter());
+text.anchor.set(0.5, 0.5);
+
+stage.addChild(text);
+
+document.body.appendChild(view);
