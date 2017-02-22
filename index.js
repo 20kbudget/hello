8ea51ef3660026683ba27bb4b9f8b1630e488b03
Fabricio C Zuardi <fabricio@fabricio.org>
2017-02-21 23:51:25 -0300

    try to run the debug always in the device

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const { Application, Text } = Pixi;
 const app = new Application();
 const { view, stage } = app;
 
+const screenCenter = () => [window.screen.width / 2, window.screen.height / 2];
+
 const text = new Text('Hello World', {
     fontFamily: 'Arial',
     fontSize: 70,
     fill: 0xffff00,
     align: 'center'
 });
-
-const screenCenter = () => [window.screen.width / 2, window.screen.height / 2];
-
 text.position.set(...screenCenter());
 text.anchor.set(0.5, 0.5);
 
