92cab4da94a00c3f840afe8e532d34c349bbf5f7
Fabricio C Zuardi <fabricio@fabricio.org>
2017-02-22 13:29:21 -0300

    minor

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Application, Text } = Pixi;
 
 const app = new Application();
 const { view, stage } = app;
+document.body.appendChild(view);
 
 const screenCenter = () => [window.screen.width / 2, window.screen.height / 2];
 
@@ -14,7 +15,5 @@ const text = new Text('Hello World', {
 });
 text.position.set(...screenCenter());
 text.anchor.set(0.5, 0.5);
-
 stage.addChild(text);
 
-document.body.appendChild(view);
